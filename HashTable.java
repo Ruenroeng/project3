ec4c7966c15896b9e41936e6671bc81a1e5070b5
Jeilanin <jgu68@wisc.edu>
2018-10-17 10:22:01 -0500

    updated but incomplete HashTable.java

--- HashTable.java
+++ HashTable.java
@@ -1,7 +1,7 @@
 /**
  * Filename:   HashTable.java
  * Project:    p3
- * Authors:    TODO: add your name(s) and lecture numbers here
+ * Authors:    TODO:*  add your name(s) and lecture numbers here
  *
  * Semester:   Fall 2018
  * Course:     CS400
@@ -16,6 +16,7 @@
 
 
 import java.util.NoSuchElementException;
+import java.util.ArrayList;
 
 // TODO: comment and complete your HashTableADT implementation
 // DO NOT ADD PUBLIC MEMBERS TO YOUR CLASS
@@ -31,21 +32,42 @@ import java.util.NoSuchElementException;
 //       you may use the hashCode provided by the <K key> object
 //       
 public class HashTable<K extends Comparable<K>, V> implements HashTableADT<K, V> {
-	
+	private ArrayList<HashTableADT<K, V>> collisionBuckets;
+	private int numBuckets;
+	private int size;
+	private double hashLoadFactor;
 	// TODO: ADD and comment DATA FIELD MEMBERS needed for your implementation
 		
 	// TODO: comment and complete a default no-arg constructor
 	public HashTable() {
+		collisionBuckets = new ArrayList<>();
+		numBuckets = 11;
+		size = 0;
+		hashLoadFactor = 0.7;
+		for (int i = 0; i<numBuckets; i++){
+			collisionBuckets.add(null);
+		}
 //a single comment
 	}
 	
 	// TODO: comment and complete a constructor that accepts initial capacity and load factor
 	public HashTable(int initialCapacity, double loadFactor) {
-
+		collisionBuckets = new ArrayList<>();
+		numBuckets = initialCapacity;
+		size = 0;
+		hashLoadFactor = loadFactor;
+		for (int i=0; i< numBuckets; i++){
+			collisionBuckets.add(null);
+		}
 	}
 
 	// TODO: comment and complete this method
 	@Override
+	private int getIndex(K key){
+		int hash = key.hashCode();
+		int index = hash % numBuckets;
+		return index;
+	}
 	public void put(K key, V value) throws IllegalArgumentException {
 
 	}
@@ -53,7 +75,7 @@ public class HashTable<K extends Comparable<K>, V> implements HashTableADT<K, V>
 	// TODO: comment and complete this method
 	@Override
 	public V get(K key) throws NoSuchElementException {
-
+		int index = getIndex(key);
 		return null;
 	}
 	
