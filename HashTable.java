998c12fc0d378ecc014ee2e34068898129e09735
jiegu <jiegu@10.0.1.80>
2018-10-21 22:19:25 -0500

    Modify HashTable.java - please check Ryan

--- HashTable.java
+++ HashTable.java
@@ -34,13 +34,14 @@ public class HashTable<K extends Comparable<K>, V> implements HashTableADT<K, V>
 	//TODO: Documentation
   private static class Node<K,V> {
 	  //Instance Variables
-	  private int hash;
-	  private V value;
-	  
+	  K key;
+	  V value;
+	  Node<K,V> next;
 	  //Constructor
-	  private Node (int hash, V value) {
-	    this.hash = hash;
+	  public Node (K key, V value) {
+	    this.key = key;
 	    this.value = value;
+	    next = null;
 	    }
 //    public String toString() {
 //      System.out.println("Key: " .toString(),"Value: ",value.toString());
@@ -53,25 +54,77 @@ public class HashTable<K extends Comparable<K>, V> implements HashTableADT<K, V>
 	}
   
   private ArrayList<Node<K,V>>[] table;
-	
+  private ArrayList<Integer> primeList = new ArrayList<Integer>();
+  private int primeIndex;
   private int numNodes;
-	private int size;
-	private double hashLoadFactor;
+  private int size;
+  private double hashLoadFactor;
 
 	// TODO: ADD and comment DATA FIELD MEMBERS needed for your implementation
 
 	// TODO: comment and complete a default no-arg constructor
 	public HashTable() {
-		this.table = new ArrayList[11];
+		primeList.add(11);
+		primeList.add(23);
+		primeList.add(47);
+		primeList.add(97);
+		primeList.add(301);
+		primeList.add(1011);
+		primeIndex = 0;
+		numNodes = primeList.get(primeIndex);
+		primeIndex ++;
+		this.table = new ArrayList[numNodes];
 		this.size = 0;
 		this.hashLoadFactor = 0.7;
+		for (int i = 0; i < numNodes; i++) { 
+            table[i] = new ArrayList<Node<K,V>>();
+            table[i].add(null);
+        } 
+		System.out.println("Hash Table Created!"); 
+        System.out.println("Number of Buckets " + numNodes); 
+        System.out.println("Load Factor : " + hashLoadFactor + "\n"); 
 	}
 	
 	// TODO: comment and complete a constructor that accepts initial capacity and load factor
 	public HashTable(int initialCapacity, double loadFactor) {
-		this.table = new ArrayList[initialCapacity];
-		size = 0;
-		hashLoadFactor = loadFactor;
+		primeList.add(11);
+		primeList.add(23);
+		primeList.add(47);
+		primeList.add(97);
+		primeList.add(301);
+		primeList.add(1011);
+		if (initialCapacity<11) {
+			primeIndex = 0;
+		}
+		else if(initialCapacity<23) {
+			primeIndex = 1;
+		}
+		else if (initialCapacity<47) {
+			primeIndex = 2;
+		}
+		else if (initialCapacity<97) {
+			primeIndex = 3;
+		}
+		else if (initialCapacity<301) {
+			primeIndex = 4;
+		}
+		else if (initialCapacity<1011) {
+			primeIndex = 5;
+		}
+		else {
+			primeIndex = 6;
+		}
+		numNodes = initialCapacity;
+		this.table = new ArrayList[numNodes];
+		this.size = 0;
+		this.hashLoadFactor = loadFactor;
+		for (int i = 0; i < numNodes; i++) { 
+            table[i] = new ArrayList<Node<K,V>>();
+            table[i].add(null);
+        } 
+		System.out.println("Hash Table Created!"); 
+        System.out.println("Number of Buckets " + numNodes); 
+        System.out.println("Load Factor : " + hashLoadFactor + "\n"); 
 		}
  
 	// TODO: comment and complete this method
@@ -113,7 +166,7 @@ public class HashTable<K extends Comparable<K>, V> implements HashTableADT<K, V>
     
     //Loop through ArrayList to try and find value.
     for (int i = 0; i < table[index].size(); i++) {
-      if (table[index].get(i).hash == hash)
+      if (table[index].get(i).key == key)
         return table[index].get(i).value;
     
     }
@@ -138,28 +191,81 @@ public class HashTable<K extends Comparable<K>, V> implements HashTableADT<K, V>
   // TODO: comment and complete this method
   
   private void putIndex(K key, V value) {
-    int hash = key.hashCode();
-    int index = hash % table.length;
-    Node<K,V> newNode = new Node<K,V>(hash,value);
-    //Needed if node has never been touched before. How can we instantiate all of the lists.
-    if (table[index] == null) {
-      //Also not adding a key/value pair.
-      table[index].add(newNode);
-      
-    }
-//    Node<K,V> currNode = table[index];
-    for (int i = 0; i < table[index].size(); i++) {
-      
-      if (table[index].get(i).hash == hash) {
-        table[index].get(i).value = value;
-        this.size++;
-        return;
-      }
-      //currNode = table[index].next;
-    }
-      table[index].add(newNode);
-    }    
+    int putindex = getIndex(key);
+    Node<K,V> firstNode = table[putindex].get(0);
+    while (firstNode != null) { 
+    	  
+        // If already present the value is updated 
+        if (firstNode.key.equals(key)) { 
+        	firstNode.value = value; 
+            return; 
+        } 
+        firstNode = firstNode.next; 
+    } 
+    Node<K,V> newNode = new Node<K,V>(key,value);
+    firstNode = table[putindex].get(0);
+    newNode.next = firstNode;
+    table[putindex].add(0,newNode);
+    System.out.println(key + ":" + value + " inserted!");
+    size++;
+    double currentLoadFactor = (1.0 * size) / numNodes; 
+    
+    System.out.println("Current Load factor = " + currentLoadFactor); 
 
+    // If the load factor is > 0.75, rehashing is done 
+    if (currentLoadFactor > hashLoadFactor) { 
+        System.out.println("Rehashing: " + currentLoadFactor); 
+        // Rehash 
+        rehash(); 
+
+        System.out.println("Number of Buckets: " + numNodes + "\n"); 
+    } 
+
+    System.out.println("Number of items: " + size); 
+    System.out.println("Size of Map: " + numNodes + "\n"); 
+  }
+
+  private void rehash() 
+  { 
+      // copy current table
+	  ArrayList<Node<K,V>>[] temp = table; 
+
+      // access new size of table
+	  if(primeIndex<6) {
+		  numNodes = primeList.get(primeIndex);
+	  }
+	  else {
+		  numNodes = numNodes*2+1;
+	  }
+	  primeIndex++;
+	  table = new ArrayList[numNodes];
+	  size = 0;
+		for (int i = 0; i < numNodes; i++) { 
+          table[i] = new ArrayList<Node<K,V>>();
+          table[i].add(null);
+      } 
+		System.out.println("Hash Table Created!"); 
+      System.out.println("Number of Buckets " + numNodes); 
+      System.out.println("Load Factor : " + hashLoadFactor + "\n");
+
+      for (int i = 0; i < temp.length; i++) { 
+
+          // head of the chain at that index 
+          Node<K, V> firstNode = temp[i].get(0); 
+
+          while (firstNode != null) { 
+              K key = firstNode.key; 
+              V val = firstNode.value; 
+
+              // calling the insert function for each node in temp 
+              // as the new list is now the bucketArray 
+              put(key, val); 
+              firstNode = firstNode.next; 
+          } 
+      } 
+
+      System.out.println("Rehashed"); 
+  } 
 	// TODO: comment and complete this method
 	@Override
 	public void remove(K key) throws NoSuchElementException {
